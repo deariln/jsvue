12217d83975276558abdc1b658c105dbdd18d1b3
deariln <deariln@mail.ru>
2024-08-08 21:11:59 +0300

    33

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
+<template>
+  <h1 v-if="bool" @click="str+='!'">{{ str }}</h1>
+  <h1 v-else @click="str+='!'" >{{ str.split('').reverse().join('') }}</h1>
+  <button v-on:click="bool=!bool">reverse</button>
+ </template>
+
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+const str = ref('learn vue')
+const bool = ref(true)
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+h1 {
+  user-select: none;
 }
 </style>
