9f74ecd29306983ef6f322e52af4a9574e8a55e8
deariln <deariln@mail.ru>
2024-09-12 21:02:47 +0300

    new

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <RouterLink to="/">На главную</RouterLink>
   <RouterLink to="/about">About</RouterLink>
+  <RouterLink to="/new">New</RouterLink>
   <RouterView></RouterView>
 
 </template>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,14 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
+import NewPage from '#root/pages/NewPage.vue'
 import HomePage from '#root/pages/HomePage.vue'
-import AboutPage from '#root/pages/AboutPage.vue'
+import AboutPage from '#root/pages/AboutPage.vue' 
+
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/about', component: AboutPage },
+  { path: '/new', component: NewPage}
 ]
 
 const router = createRouter({
