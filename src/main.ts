c4bb90fd47521e1597ab6dbbe2a09b47c238296c
deariln <deariln@mail.ru>
2024-10-01 21:09:55 +0300

    p

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from '#root/App.vue'
+import {createPinia} from 'pinia'
 import {router} from '#root/routes'
 
+
+const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
 // app.component('HeaderComp', HeaderComp)
 app.mount('#app')
+app.use(pinia)
